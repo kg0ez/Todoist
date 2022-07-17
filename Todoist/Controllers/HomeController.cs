cf99d4ce82341a4b7031d8c42108c8f05e3cca24
kg0ez <82456927+kg0ez@users.noreply.github.com>
2022-07-17 12:51:45 +0300

    Todoist
    First version Todoist

---
+++ Todoist/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Todoist.Models;
+
+namespace Todoist.Controllers;
+
+public class HomeController : Controller
+{
+    private ApplicationContext _db;
+
+    public HomeController(ApplicationContext context)=>
+        _db = context;
+
+    public async Task<IActionResult> Index()=>
+        View(await _db.Tasks.ToListAsync());
+
+    public IActionResult CreateTask()=>
+        View();
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTask(Models.Task task)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Tasks.Add(task);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        return View(task);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id != null)
+        {
+            Models.Task? task = await _db.Tasks.FirstOrDefaultAsync(p => p.Id == id);
+            if (task != null)
+            {
+                _db.Tasks.Remove(task);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+        }
+        return NotFound();
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id != null)
+        {
+            Models.Task? task = await _db.Tasks.FirstOrDefaultAsync(p => p.Id == id);
+            if (task != null) return View(task);
+        }
+        return NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(Models.Task task)
+    {
+        _db.Tasks.Update(task);
+        await _db.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+}
+
---
+++ Todoist/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todoist.Models
+{
+	public class Task
+	{
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        public string? Subtitle { get; set; }
+
+        public DateTime FinalData { get; set; }
+    }
+}
+
